436fbaed6b17ffa06c1b057665b3be1e0b83c432
Jose Arenas <jo.arenas96@gmail.com>
2018-02-02 12:02:36 -0600

    Sorty class
    metodos vacios creados

---
+++ Sorting/src/sorting/Sorty.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorting;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jose Arenas
+ */
+public class Sorty {
+    
+    
+   
+    
+    /**
+     * 
+     * @param L
+     * @return 
+     */
+    public List<Comparable> gnomeSort(List<Comparable> L)
+    {
+        return null;     
+    }
+    
+   
+    /**
+     * 
+     * @param L
+     * @return 
+     */
+    public List<Comparable> mergeSort(List<Comparable> L)
+    {
+        return null;
+    }
+    
+    
+    /**
+     * 
+     * @param L
+     * @return 
+     */
+    public List<Comparable> quickSort(List<Comparable> L)
+    {
+        return null;
+    }
+    
+    
+    /**
+     * 
+     * @param L
+     * @return 
+     */
+    public List<Comparable> radixSort(List<Comparable> L)
+    {
+        return null;
+    }
+      
+}
